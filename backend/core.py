8a6be6508c8d16ec4b9369691e76446040064fdc
sanyogita <sanyogitavkaps@gmail.com>
2024-01-19 12:38:52 +0500

    Add static prompt and some changes for css

--- backend/core.py
+++ backend/core.py
@@ -1,17 +1,11 @@
+
 import os
 from typing import Any, Dict, List
-
-# from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_openai.embeddings import OpenAIEmbeddings
-# from langchain.chat_models import ChatOpenAI
-# from langchain_community.chat_models import ChatOpenAI
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-# from langchain.vectorstores import Pinecone
 from langchain_community.vectorstores import Pinecone
 import pinecone
-
-
 
 pinecone.init(
     api_key=os.environ["PINECONE_API_KEY"],
@@ -20,7 +14,18 @@ pinecone.init(
 
 INDEX_NAME = "website-doc"
 
+# Define your static prompts and responses
+static_prompts = {
+    "Do you have radiant heat flooring?": "Yes, we do, please see here: https://www.decolandia.ro/parchet-pentru-incalzire-prin-pardoseala",
+    "Do you have engineered flooring, 10 mm thick, tongue and grove?": "Yes, we have. Please check here: https://www.decolandia.ro/parchet-stratificat/filtrare/grosime-f3,10-mm-v25/tip-prindere-f6,nut-si-feder-v45",
+    "which one is in stock?": "For these products, we only have stock at the external provider, meaning a delivery term of 5-10 days for https://www.decolandia.ro/parchet-stratificat/filtrare/grosime-f3,10-mm-v25/tip-prindere-f6,nut-si-feder-v45?stoc=1",
+}
+
 def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+    # Use the static response if the query is in the static prompts
+    if query in static_prompts:
+        return {"answer": static_prompts[query], "source_documents": []}
+
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
     docsearch = Pinecone.from_existing_index(
         embedding=embeddings,
@@ -37,4 +42,3 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     )
 
     return qa({"question": query, "chat_history": chat_history})
-
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
-
-
 from typing import Set
 from backend.core import run_llm
 import streamlit as st
 from streamlit_chat import message
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
@@ -16,38 +16,31 @@ def create_sources_string(source_urls: Set[str]) -> str:
         sources_string += f"{i+1}. {source}\n"
     return sources_string
 
-st.markdown("<h1>🤖 Website Read Chat bot</h1>", unsafe_allow_html=True)
+st.header("Website Read Chat bot", divider='rainbow')
 
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
     st.session_state["user_prompt_history"] = []
     st.session_state["chat_history"] = []
-    st.session_state["chatbot_open"] = False
-
-chat_button_clicked = st.button("Chat")  # Button to toggle the chatbot
-
-if chat_button_clicked:
-    st.session_state["chatbot_open"] = not st.session_state.get("chatbot_open", False)
+    st.session_state["chatbot_open"] = True  # Set to True directly to open the chatbot
 
 if st.session_state["chatbot_open"]:
-    # prompt = st.text_input("", placeholder="Enter your message here...")
-    with st.form(key='my_form', clear_on_submit=True):
-        prompt = st.text_area("You:", key='input', height=50)
-        submit_button = st.form_submit_button(label='Send')
+    prompt = st.chat_input("What is up?")
     if prompt:
-            with st.spinner("Generating response..."):
-                generated_response = run_llm(
-                    query=prompt, chat_history=st.session_state["chat_history"]
-                )
-                sources = set(
-                    [doc.metadata["source"] for doc in generated_response["source_documents"]]
-                )
-                response_message = f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
+        with st.spinner("Generating response..."):
+            generated_response = run_llm(
+                query=prompt, chat_history=st.session_state["chat_history"]
+            )
 
-            # Update chat history after response generation
-            st.session_state.chat_history.append((prompt, generated_response["answer"]))
-            st.session_state.user_prompt_history.append(prompt)
-            st.session_state.chat_answers_history.append(response_message)
+            sources = set(
+                [doc.metadata["source"] for doc in generated_response["source_documents"]]
+            )
+            response_message = f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
+
+        # Update chat history after response generation
+        st.session_state.chat_history.append((prompt, generated_response["answer"]))
+        st.session_state.user_prompt_history.append(prompt)
+        st.session_state.chat_answers_history.append(response_message)
 
     if st.session_state["chat_answers_history"]:
         for generated_response, user_query in zip(
